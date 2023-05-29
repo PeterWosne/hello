f405c535b5f74edfdfa7f474925f9f666191f865
PeterWosne <pwosne@gmail.com>
2023-05-29 16:45:19 +0300

    Initial commit

---
+++ src/main/java/ru/ermakow/WebInitializer.java
@@ -0,0 +1,19 @@
+package ru.ermakow;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
